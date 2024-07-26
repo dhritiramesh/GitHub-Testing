57bbb7f1ea882f8d3e4fab2922ebf7a39dcad454
Dhriti Ramesh <dhritiramesh2008@gmail.com>
2024-07-26 11:01:04 -0600

    Initial Commit

---
+++ GitHub Testing/GitHub_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestingApp.swift
+//  GitHub Testing
+//
+//  Created by Dhriti Ramesh on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
